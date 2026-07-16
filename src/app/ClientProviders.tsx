43c573ff151fc1df99d631a1ee47b9ac75bfeb51
abelwebdev <82320458+abelwebdev@users.noreply.github.com>
2026-07-16 19:53:37 +0300

    feat: added posthog

--- src/app/ClientProviders.tsx
+++ src/app/ClientProviders.tsx
@@ -1,11 +1,25 @@
 'use client';
 
+import { useEffect } from 'react';
+import posthog from 'posthog-js';
+
 import { ThemeProvider } from "@/components/theme-provider";
 import { Provider } from "react-redux";
 import { store } from "@/store";
 import { PlayerProvider } from "@/contexts/PlayerContext";
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
+      api_host: '/ingest',
+      ui_host: 'https://us.posthog.com',
+      capture_pageview: false,
+      loaded: (posthog) => {
+        console.log('PostHog loaded:', posthog);
+      },
+    });
+  }, []);
+
   return (
     <ThemeProvider
       attribute="class"
